f5f9989d2bcff0f99cbe987469aa8470d6686693
michawiet <michawiet@gmail.com>
2021-05-19 16:08:01 +0200

    Disabling font scaling

--- src/main/java/scenes/HelpSceneController.java
+++ src/main/java/scenes/HelpSceneController.java
@@ -1,11 +1,7 @@
 package scenes;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
 import javafx.scene.text.FontSmoothingType;
-import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 public class HelpSceneController {
@@ -14,15 +10,12 @@ public class HelpSceneController {
 
     public void load(String helpFileName) {
         webView.getEngine().load(getClass().getResource(helpFileName).toExternalForm());
-        //webView.getEngine().load(getClass().getResource("/help/index.html").toExternalForm());
     }
 
     @FXML
     void initialize() {
         webView.contextMenuEnabledProperty().setValue(false);
         webView.fontSmoothingTypeProperty().setValue(FontSmoothingType.GRAY);
-        webView.setFontScale(1.15);
-        //webView.getEngine().load(getClass().getResource("/help/index.html").toExternalForm());
     }
 
 }
